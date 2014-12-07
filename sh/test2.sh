a182557ac3d63e3845a6cb7636e2c9ea68a9d992
anush0247 <anush0247@gmail.com>
2014-12-07 14:06:52 +0530

    learning sh

---
+++ sh/test2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read x
+read y
+
+if [ $x ge $y]
+then
+    echo "$x is greater than $y"
+else if [$x ]
+
+#echo "$(($x+$y))"
+#echo "$(($x-$y))"
+#echo "$(($x*$y))"
+#echo "$(($x/$y))"
