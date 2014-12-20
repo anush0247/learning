2d9d2df1322da8246be8fee192f928363a51cef7
anush0247 <anush0247@gmail.com>
2014-12-20 11:01:01 +0530

    learning node.js https

---
+++ node/app1/https_server.js
@@ -0,0 +1,15 @@
+
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+    key: fs.readFileSync('privatekey.pem'),
+    cert: fs.readFileSync('certificate.pem')
+    };
+
+https.createServer(options, function (req, res) {
+res.writeHead(200);
+res.write("https!\n");
+res.end();
+}).listen(8080); 
+
